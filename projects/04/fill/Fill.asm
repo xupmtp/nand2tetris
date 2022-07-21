817a49faca36371ba154a470a914678b0c29eb2c
simon10539 <simoniii10539@i-mps.com>
2022-07-21 10:20:44 +0800

    project 4 新增註解

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,7 +10,15 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
+// ===========以下偽代碼==========
+// while true:
+//     color=0, i=0;
+//     D = RAM[24576]; // 取得鍵盤輸入
+//     if D > 0:
+//         color=-1;
+//     for (;i<8192;): // 渲染螢幕
+//         RAM[@SCREEN+i] = color;
+//         i++;
 (LOOP)
     @color
     M=0
@@ -32,12 +40,14 @@
     0;JMP
 
 (WRITE_LOOP)
+    // 條件
     @i
     D=M
     @8192
     D=D-A
     @LOOP
     D;JGE
+    // 渲染
     @SCREEN
     D=A
     @i
@@ -49,6 +59,7 @@
     @addr
     A=M
     M=D
+    // i++
     @i
     M=M+1
     @WRITE_LOOP
